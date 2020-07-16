b4a612f2a3afd1d77a9d28e60cd3d759215276be
barrielui <luishingyuen@gmail.com>
2020-07-16 12:05:41 +1200

    Remove legacy API usage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import MetaTags from 'react-meta-tags';
 import './App.css';
 import FormBody from './FormBody';
 import AboutBody from './AboutBody';
@@ -11,29 +10,27 @@ import {
 } from "react-router-dom";
 
 class App extends React.Component {
+  componentDidMount() {
+    document.title = "Bee Card Balance Checker"
+  }
+  
   render() {
     return (
     <div className="App">
-      <MetaTags>
-        <title>Bee Card Balance Checker</title>
-      </MetaTags>
       <Router>
         <div className="App-header">
           <h1> <Link to="/" className="App-title">Bee Card Balance Checker</Link></h1>
         </div>
-        
         <Switch>
           <Route exact path="/">
             <FormBody />
           </Route>
-          
           <Route exact path="/about">
             <AboutBody />
           </Route>
         </Switch>
         </Router>
       </div>
-      
     );
   }
 }
