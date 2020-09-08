ca0ea9cf40621f73cc09d4addf844496e76e01f5
barrielui <54468342+barrielui@users.noreply.github.com>
2020-09-09 11:30:38 +1200

    New tab for some hyperlinks

--- src/AboutBody.js
+++ src/AboutBody.js
@@ -18,13 +18,13 @@ class FormBody extends React.Component{
         <br/>
         <h1>About</h1>
         <span className="About-body">
-            <p className="About-content">This website serve as a shortcut to check Bee Card balance.  The request is redirected to and processed by Bee Card official website.  No data is processed nor stored on Heroku server.  Feel free to inspect the source code of the website on <a href="https://github.com/barrielui/self-bee-card-balance-checker-web">GitHub</a>. 
+            <p className="About-content">This website serve as a shortcut to check Bee Card balance.  The request is redirected to and processed by Bee Card official website.  No data is processed nor stored on Heroku server.  Feel free to inspect the source code of the website on <a href="https://github.com/barrielui/self-bee-card-balance-checker-web" target="_blank">GitHub</a>. 
             
             <br/><br/>
             The website uses cookies to store your card number to facilitate balance checking.  Security code is not saved.  Op-out option work in process. Click <a href="/#" onClick={this.removeCookies}>here</a> to remove cookies.
             
             <br/><br/>
-            This website is developed in compliance with the <a href="https://beecard.co.nz/Pages/Terms">copyright statement</a> on Bee website.  Usage of the website and further development should comply with those terms.
+            This website is developed in compliance with the <a href="https://beecard.co.nz/Pages/Terms" target="_blank">copyright statement</a> on Bee website.  Usage of the website and further development should comply with those terms.
             </p>
         </span>
         
